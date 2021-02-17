2467018fd3a8147d67129a2242ebaac698e7ad23
Jason-Kyle De Lara <jason8oh8@gmail.com>
2021-02-16 19:24:48 -0800

    Facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     secretKey: "12345-67890-09876-54321",
     mongoUrl: "mongodb://localhost:27017/nucampsite",
+    facebook: {
+        clientId: "693202718023697",
+        clientSecret: "e8dd2328c216185f7cc24716ecf9032b",
+    },
 };
